5d95b2423be3a4944f7d2bbd4318d4fabd380373
Imre Fazekas <imre.fazekas@icloud.com>
2018-09-05 17:57:11 +0200

    fixed address regexp

--- dummyTest.js
+++ dummyTest.js
@@ -1,10 +1,10 @@
 let name = /^[a-zA-Z\u00C0-\u017F]+\.?\s?[a-zA-Z\u00C0-\u017F]+[.,-]?\.?\s?[a-zA-Z\u00C0-\u017F]+[.,-]?$/
 
-let address = /^[a-zA-Z\u00C0-\u017F0-9.,\s]+$/
+let address = /^[a-zA-Z\u00C0-\u017F0-9-.,\s]+$/
 
 let password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\u00C0-\u017F!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{8,32}$/
 
 console.log( name.test( 'Hajdú' ) )
 console.log( name.test( 'Hajdú-Bíhar' ) )
-console.log( address.test( 'Thomas Mann u. 31' ) )
+console.log( address.test( 'Hajdú-Bihar' ) )
 console.log( password.test( 'Start123' ) )
