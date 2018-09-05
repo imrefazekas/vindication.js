8235575100abcfc586c4d2595a67ab672fc5312b
Imre Fazekas <imre.fazekas@icloud.com>
2018-09-05 17:32:00 +0200

    corrected passwordű

--- dummyTest.js
+++ dummyTest.js
@@ -2,6 +2,9 @@ let name = /^[a-zA-Z\u00C0-\u017F]+\.?\s?[a-zA-Z\u00C0-\u017F]+[.,-]?\.?\s?[a-zA
 
 let address = /^[a-zA-Z\u00C0-\u017F0-9.,\s]+$/
 
+let password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\u00C0-\u017F!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{8,32}$/
+
 console.log( name.test( 'Hajdú' ) )
 console.log( name.test( 'Hajdú-Bíhar' ) )
 console.log( address.test( 'Thomas Mann u. 31' ) )
+console.log( password.test( 'Start123' ) )
